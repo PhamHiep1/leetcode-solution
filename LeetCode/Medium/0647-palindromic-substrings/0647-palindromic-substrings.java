c6d3e679de2c0681f8ff6a2ae27ce39a5f78c505
PhamHiep1 <diepthu19062006@gmail.com>
2026-06-05 10:26:27 +0700

    5 ms (97.09%) 42.2 MB (99.73%) Two Pointers

--- LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
+++ LeetCode/Medium/0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,21 +1,24 @@
 class Solution {
-    private int count = 0;
-    // babad
-    // 5, bab, aba
+
     public int countSubstrings(String s) {
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        int count = 0;
         for(int i = 0; i<s.length(); i++){
-            expandAroundCenter(s,i,i);
-            expandAroundCenter(s,i,i+1);
+            count += expandAroundCenter(s,i,i);
+            count += expandAroundCenter(s,i,i+1);
         }
         
         return count;
     }
-    private void expandAroundCenter(String s, int l, int r){
+    private int expandAroundCenter(String s, int l, int r){
+        int count = 0;
         while(l >=0 && r<s.length() && s.charAt(l) == s.charAt(r)){
             l--;
             r++;
             count++;
         }
-
+        return count;
     }
 }
