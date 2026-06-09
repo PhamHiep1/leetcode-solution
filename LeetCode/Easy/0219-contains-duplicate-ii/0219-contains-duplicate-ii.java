6c002769342c1baee4f6a963b8533745afbff4ce
PhamHiep1 <diepthu19062006@gmail.com>
2026-06-09 20:31:44 +0700

    28 ms (15.82%) 75.9 MB (90.2%) Array

---
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if(k < 0 || nums == null || nums.length <= 1){
+            return false;
+        }
+
+        Set<Integer> window = new HashSet<>();
+
+        for(int right = 0; right < nums.length; right++){
+            if(window.contains(nums[right])){
+                return true;
+            }
+
+            window.add(nums[right]);
+            
+            if(window.size() > k){
+                window.remove(nums[right - k]);
+            }
+        }
+        return false;
+    }
+}
