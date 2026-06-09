538077a91abe7d22ec4f55a9098c4398f260225c
PhamHiep1 <diepthu19062006@gmail.com>
2026-06-09 20:36:20 +0700

    19 ms (97.74%) 80.7 MB (57.23%) Array

--- LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ LeetCode/Easy/0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -7,14 +7,12 @@ class Solution {
         Set<Integer> window = new HashSet<>();
 
         for(int right = 0; right < nums.length; right++){
-            if(window.contains(nums[right])){
-                return true;
+            if(right > k){
+                window.remove(nums[right - k - 1]);
             }
 
-            window.add(nums[right]);
-            
-            if(window.size() > k){
-                window.remove(nums[right - k]);
+            if(!window.add(nums[right])){
+                return true;
             }
         }
         return false;
