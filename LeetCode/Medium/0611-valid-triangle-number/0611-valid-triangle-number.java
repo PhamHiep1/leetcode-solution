bc8fa397ef95d2508e1429428a07432df3b47f85
PhamHiep1 <diepthu19062006@gmail.com>
2026-06-10 09:19:45 +0700

    29 ms (29.43%) 45.7 MB (59.19%) Array

--- LeetCode/Medium/0611-valid-triangle-number/0611-valid-triangle-number.java
+++ LeetCode/Medium/0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -3,13 +3,13 @@ class Solution {
         if(nums == null || nums.length <3){
             return 0;
         }
-
         
         int count = 0;
         Arrays.sort(nums);
-        
-        for(int c = nums.length-1; c >= 2; c--){
-            int b = c-1;
+        int n = nums.length;
+
+        for(int c = n - 1; c >= 2; c--){
+            int b = c - 1;
             int a = 0;
             while(a < b){
                 if(nums[a] + nums[b] > nums[c]){
