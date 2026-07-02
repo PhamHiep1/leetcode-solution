2d97fdc837c21e22a5bead422af6147d35cbb117
PhamHiep1 <diepthu19062006@gmail.com>
2026-07-02 15:56:39 +0700

    2 ms (95.99%) 82.1 MB (30.27%) Tree

--- LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ LeetCode/Easy/0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -13,21 +13,35 @@
  *     }
  * }
  */
-
 class Solution {
     public int minDepth(TreeNode root) {
         if(root == null){
             return 0;
         }
-        
-        if(root.left == null){
-            return 1 + minDepth(root.right);
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int minDepth = 1;
+
+        while(!q.isEmpty()){
+            int levelSize = q.size();
+            for(int i = 0; i < levelSize; i++){
+                TreeNode curr = q.poll();
+                if(curr.left == null && curr.right == null){
+                    return minDepth ;
+                }
+
+                if(curr.left != null){
+                    q.offer(curr.left);
+                }
+
+                if(curr.right != null){
+                    q.offer(curr.right);
+                }
+            }
+            minDepth++;
         }
-        
-        if(root.right == null){
-            return 1 + minDepth(root.left);
-        }
- 
-        return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+
+        return minDepth;
     }
 }
